9e43c2de1ae100193af32251d829d1cc4df5c53d
abhilashakarru65-commits <abhilashakarru65@gmail.com>
2026-06-12 13:23:42 +0530

    Delete List_&_Tuple.py

--- List_&_Tuple.py
+++
@@ -1,7 +0,0 @@
-# List of 5 Fruits
-fruits = ["Apple", "Banana", "Mango", "Orange", "Grapes"]
-print("Fruits:", fruits)
-
-# Tuple of 3 colors & printing the first color
-colors = ("Red", "Blue", "Green")
-print("First Color:", colors[0])

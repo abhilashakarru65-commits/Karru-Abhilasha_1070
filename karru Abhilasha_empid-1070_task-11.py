fa3893e99dab97a842ba43c156e784306d1beec4
abhilashakarru65-commits <abhilashakarru65@gmail.com>
2026-06-14 12:55:04 +0530

    DAY-11 Task

--- karru Abhilasha_empid-1070_task-11.py
+++ karru Abhilasha_empid-1070_task-11.py
@@ -1,43 +1,58 @@
-# ==================================
-# BUG 1 - RUNTIME ERROR
-# ==================================
-# Type   : IndexError
-# Reason : Wrong list position
-# Fix    : Use a valid position
+import csv
 
-print("BUG 1 - RUNTIME ERROR")
+# Writing data to CSV file
+file = open("student.csv", "w", newline="")
+writer = csv.writer(file)
 
-try:
-    marks = [80, 75, 90, 65, 85]
-    print(marks[5])   # Buggy Code
-except IndexError as e:
-    print("Error:", e)
+writer.writerow(["RollNo", "Name", "Sub1", "Sub2", "Sub3", "Sub4"])
 
-# Fixed Code
-print("Fixed Output:", marks[0])
+writer.writerow([1, "Ram", 80, 85, 90, 88])
+writer.writerow([2, "Sita", 75, 80, 85, 90])
+writer.writerow([3, "Ravi", 90, 92, 95, 94])
+writer.writerow([4, "Priya", 85, 88, 84, 86])
+writer.writerow([5, "Arjun", 70, 75, 80, 78])
+writer.writerow([6, "Sneha", 88, 90, 92, 89])
+writer.writerow([7, "Kiran", 78, 82, 80, 79])
+writer.writerow([8, "Anu", 95, 96, 94, 97])
+writer.writerow([9, "Rahul", 82, 84, 86, 88])
+writer.writerow([10, "Divya", 89, 91, 90, 92])
 
+file.close()
 
-# ==================================
-# BUG 2 - LOGICAL ERROR
-# ==================================
-# Type   : Logical Error
-# Reason : Wrong average formula
-# Fix    : Divide by total students
+# Reading and displaying records
+file = open("student.csv", "r")
+reader = csv.reader(file)
 
-print("\nBUG 2 - LOGICAL ERROR")
+print("Student Records:")
+for row in reader:
+    print(row)
 
-total = sum(marks)
+file.close()
 
-# Buggy Code
-wrong_average = total / 4
-print("Wrong Average:", wrong_average)
+# Finding highest scorer and average marks
+file = open("student.csv", "r")
+reader = csv.reader(file)
+next(reader)
 
-# Fixed Code
-average = total / len(marks)
-highest = max(marks)
-lowest = min(marks)
+highest_name = ""
+highest_total = 0
+grand_total = 0
+count = 0
 
-print("Correct Average:", average)
-print("Total Marks:", total)
-print("Highest Marks:", highest)
-print("Lowest Marks:", lowest)
+for row in reader:
+    total = int(row[2]) + int(row[3]) + int(row[4]) + int(row[5])
+
+    if total > highest_total:
+        highest_total = total
+        highest_name = row[1]
+
+    grand_total += total
+    count += 1
+
+print("\nHighest Scorer:", highest_name)
+print("Total Marks:", highest_total)
+
+average = grand_total / count
+print("Average Marks of All Students:", average)
+
+file.close()
